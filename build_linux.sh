724041d18f7ac89105dc813448f6a1f04423dea5
kumori0nomori <kumori0nomori@gmail.com>
2026-08-14 15:07:11 +0900

    #5: rename build.sh

--- build_linux.sh
+++ build_linux.sh
@@ -1,7 +1,3 @@
-#
-# GUI
-#
-
 # CPU-only build (does not require CUDA Toolkit or an NVIDIA GPU)
 cmake --preset linux-cpu-debug
 cmake --build --preset linux-cpu-debug --target format
@@ -9,6 +5,7 @@ cmake --build --preset linux-cpu-debug
 ctest --preset linux-cpu-debug --output-on-failure
 ./build/linux-cpu-debug/raypalette_gui
 
+# CUDA build (requires CUDA Toolkit and an NVIDIA GPU)
 cmake --preset linux-debug
 cmake --build --preset linux-debug --target format
 cmake --build --preset linux-debug
