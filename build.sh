58f802a1aa6cb2b0f42cfd65d168d64815e9468d
kumori0nomori <kumori0nomori@gmail.com>
2026-08-14 12:56:24 +0900

    #5: enable cpu-gui

--- build.sh
+++ build.sh
@@ -19,9 +19,11 @@ ctest --preset linux-cpu-debug --output-on-failure
 #
 
 # CPU-only build (does not require CUDA Toolkit or an NVIDIA GPU)
-# cmake --preset linux-cpu-debug
-# cmake --build --preset linux-cpu-debug --target format
-# ctest --preset linux-cpu-debug --output-on-failure
+cmake --preset linux-cpu-gui-debug
+cmake --build --preset linux-cpu-gui-debug --target format
+cmake --build --preset linux-cpu-gui-debug
+ctest --preset linux-cpu-gui-debug --output-on-failure
+./build/linux-cpu-gui-debug/raypalette_gui
 
 cmake --preset linux-gui-debug
 cmake --build --preset linux-gui-debug --target format
